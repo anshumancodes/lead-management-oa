fa361d295f23f984b9f5a8b4ae490f7e6a4fc6ec
anshumancodes <anshumanprof01@gmail.com>
2026-05-20 11:28:36 +0530

    feat(auth): add auth routes

---
+++ server/src/routes/auth.route.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { authController } from '../controllers/auth.controller.js';
+import { verifyJWT } from '../common/middlewares/auth.js';
+
+const router = Router();
+
+router.post('/register', authController.register);
+router.post('/login',    authController.login);
+router.post('/logout',   verifyJWT, authController.logout);
+router.get('/me',        verifyJWT, authController.me);
+
+export default router;
